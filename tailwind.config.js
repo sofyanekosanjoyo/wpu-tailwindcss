341e554d1490b9208ac42b30e8051be91e504b2e
Sofyan Eko Sanjoyo <sofyanekosanjoyo@pu.go.id>
2022-12-20 16:49:19 +0700

    Functions & Directives Pada Tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        13: "3.25rem",
+      },
+      fontFamily: {
+        inter: ["Inter"],
+      },
+      colors: {
+        clifford: "#da373d",
+        warnaBebas: "#bada55",
+        kopi: "#c0ffee",
+      },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+        goyang: "goyang 1s ease-in-out infinite",
+      },
+      keyframes: {
+        goyang: {
+          "0%, 100%": { transform: "rotate(-15deg)" },
+          "50%": { transform: "rotate(15deg)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
